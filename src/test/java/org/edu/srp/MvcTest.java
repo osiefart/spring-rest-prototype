b34c1f6fcdb6edffccb03de074db8ce016ee3885
Olaf Siefart <Olaf.Siefart@web.de>
2017-04-29 10:08:31 +0200

    initial version with spring rest doc and spring cloud contract testing

---
+++ src/test/java/org/edu/srp/MvcTest.java
@@ -0,0 +1,16 @@
+package org.edu.srp;
+
+import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+
+/**
+ * Created by osiefart on 29.04.17.
+ */
+public class MvcTest {
+
+    @Before
+    public void setup() {
+        //RestAssuredMockMvc.standaloneSetup(new FraudDetectionController());
+    }
+
+}
